f6465094d4a65d04d3ff68f059fdc41387cc4731
Rodrigo Silva dos Anjos <89110372+RodrigoAnjos2004@users.noreply.github.com>
2021-11-28 23:55:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+// card do EXODIA , bem facil de se fazer , com apenas html e css ... a verdade EXODIA é foda.
+// (a parte de trás deu um bug mas enfim bateu... a preguiça e não arrumei)
+// tempo: 53 min
